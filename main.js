1aba82c1eba08b95ac1ef4b423c03689a996f155
GABRIEL SANTANA SILVA <u21429@LAPA21.academico.cotuca.unicamp.br>
2022-06-14 13:59:59 -0700

    POST Method

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-const { response } = require('express')
 const express = require('express')
 const cors = require('cors')
-const path = require('path')
 
 const app = express()
+const path = require('path')
 
-app.use(express.urlencoded({ 
-    extended: true 
-}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json())
 app.use(express.json({ type: 'application/vnd.api+json'}))
 app.use(cors())
 
+// exemplo do uso do get
+// http://localhost:3000/
 app.get('/', (_req, res) => {
     res.statusCode = 200
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-function errorHandler(error, res) {
-    if (!error) return 'pablo'
+// POST -> serve para incluir dados no servidor
+// curl -d "key=chave&key2=chave2" -X POST http://localhost:3000/api
+app.post('/api', (req, res) => {
+    console.log("Foi mandado : \n", req.body)
+    _key = req.body.key
+    _key2 = req.body.key2
 
-    console.log('Erro ao achar a pagina')
-    res.end(JSON.stringify({
-        error: "page not found"
-    }))
-}
-
+    res.status(200).send({
+        sucess: 'true',
+        message: 'Ok correto...'
+    })
+})
 
 const hostname = 'localhost'
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-    console.log(`Servidor rodando no http://${hostname}:${port}/\n>`)
-})
+app.listen(port, () => console.log(`Servidor rodando no http://${hostname}:${port}/\n>`))
